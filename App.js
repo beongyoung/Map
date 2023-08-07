d3155e476828136f48cb87d88e96dab0700490a1
NachoCookie <sergiopark10373@likelion.org>
2023-08-07 18:39:42 +0900

    kakao map on React Native

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { WebView } from "react-native-webview";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const KakaoMapScreen = () => {
+  const kakaoMapHTML = `
+  <html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=159ef546a4fa251bbd1bb59f5bcbf9ef"></script>
+  </head>
+  <body>
+    <div id="map" style="width: 100%; height: 100vh;"></div>
+    <script>
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(37.5665, 126.9780),
+        level: 3,
+      };
+      const map = new kakao.maps.Map(container, options);
+      // Add more map-related code here...
+    </script>
+  </body>
+  </html>
+  `;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <WebView source={{ html: kakaoMapHTML }} style={{ flex: 1 }} />;
+};
+
+export default KakaoMapScreen;
